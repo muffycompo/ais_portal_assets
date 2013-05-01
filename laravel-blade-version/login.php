b848f9b8917d406deef1944bd46dc2c745917e6f
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2013-05-01 20:41:05 +0100

    Converted Login form to Laravel Blade syntax

---
+++ laravel-blade-version/login.php
@@ -0,0 +1,32 @@
+@layout('template.layout')
+
+@section('content')
+		
+<div class="home">
+	<h2>Login</h2>
+	@include('template.partials._notification')
+	{{ Form::open() }}
+	<form action="">
+		<p>
+			{{ Form::label('username','Username:') }}<br>
+			{{ Form::input('text','username','',array('id'=>'username')) }}
+		</p>
+		<p>
+			{{ Form::label('password','Password:') }}<br>
+			{{ Form::password('password',array('id'=>'password')) }}
+		</p>
+		<p>
+			{{ Form::checkbox('remember',1) }}&nbsp;Remember me.
+		</p>
+		<p>	
+			{{ Form::submit('Login') }}	| {{ HTML::link('#','Signup') }}	
+		</p>
+		<p>
+			{{ HTML::link('#','Forgot Password?') }}
+		</p>
+		{{ Form::close() }}
+	</form>
+</div>
+
+@endsection
+	
