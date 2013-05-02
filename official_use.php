38bb92865abdb16a0f510e621df2f90fae26a6f9
Williams Alfred Onen <kiwixcompome@googlemail.com>
2013-05-02 22:35:20 +0100

    I just added the entrance examination and official use pages

---
+++ official_use.php
@@ -0,0 +1,46 @@
+<?php include('template_header.php'); ?>
+		
+<div class="home">
+	<h2>Official Use</h2>
+	<form action="">
+		<p>
+			<label for="user">User:</label><br>
+			<select name="" id="">
+				<option value=""></option>
+			</select>
+		</p>
+		<p>
+			<label for="aic">AIC:</label><br>
+			<select name="" id="">
+				<option value=""></option>
+			</select>
+		</p>
+		<p>
+			<label for="l_class">Last Class:</label><br>
+			<select name="" id="">
+				<option value=""></option>
+			</select>
+		</p>
+		<p>
+			<label for="p_class">Present Class:</label><br>
+			<select name="" id="">
+				<option value=""></option>
+			</select>
+		</p>
+		<p>
+			<label for="admission_r">Admission Recomendation:</label><br>
+			<select name="" id="">
+				<option value=""></option>
+			</select>
+		</p>
+		<p>
+			<label for="admission_n">Admission Number:</label><br>
+			<input type="text" name="admission_n" id="admission_n"/>
+		</p>
+		<p>			
+			<input type="submit" name="submit" value="Add"/>
+		</p>
+	</form>
+</div>
+<?php include('template_footer.php'); ?>
+	
